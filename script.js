6278dd626ce002f6ba14b991ab4b037ca2a4b028
new-sorghum1 <rajeshvihan86@gmail.com>
2026-04-06 20:25:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function searchPage() {
     if (input.includes("cut")) window.location.href = "definition.html";
     else if (input.includes("symptom")) window.location.href = "symptoms.html";
     else if (input.includes("step")) window.location.href = "steps.html";
+    else if (input.includes("prevent")) window.location.href = "prevention.html";
     else if (input.includes("kit")) window.location.href = "kit.html";
     else alert("No results found");
 }
